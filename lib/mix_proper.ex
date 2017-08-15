6ec4d04a7ae5cfd6b7f8dfccc5f53c9512d9df93
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-08-15 20:04:12 +0200

    $ mix new mix_proper

---
+++ lib/mix_proper.ex
@@ -0,0 +1,18 @@
+defmodule MixProper do
+  @moduledoc """
+  Documentation for MixProper.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixProper.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_proper_test.exs
@@ -0,0 +1,8 @@
+defmodule MixProperTest do
+  use ExUnit.Case
+  doctest MixProper
+
+  test "greets the world" do
+    assert MixProper.hello() == :world
+  end
+end
